671d820b959f5bd16086a4e44908cfe5f1b75c24
Parzival8971 <esh8971@gmail.com>
2023-02-18 03:40:39 +0900

    Design(home): clock, productCard, productsList section styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,100 @@
 import React from 'react';
+import './footer.css';
+
+import { Col, Container, ListGroup, ListGroupItem, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className='footer'>
+      <Container>
+        <Row>
+          <Col lg='4'>
+            <div className='logo'>
+              <div>
+                <h1 className='text-white'>리액트마켓</h1>
+              </div>
+            </div>
+            <p className='footer__text mt-4'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              nemo aspernatur architecto ut, sed voluptates quaerat voluptatibus
+              doloremque incidunt tempora?
+            </p>
+          </Col>
+          <Col lg='3'>
+            <div className='footer__quick-links'>
+              <h4 className='quick__links-title'>카테고리 메뉴</h4>
+              <ListGroup className='mb-3'>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>핸드폰</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>소파</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>의자</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>스마트워치</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className='footer__quick-links'>
+              <h4 className='quick__links-title'>링크</h4>
+              <ListGroup className='mb-3'>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='/shop'>쇼핑</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='/cart'>장바구니</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='/login'>로그인</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>개인정보처리방침</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg='3'>
+            <div className='footer__quick-links'>
+              <h4 className='quick__links-title'>연락처</h4>
+              <ListGroup className='footer__contact'>
+                <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-2'>
+                  <span>
+                    <i className='ri-map-pin-line'></i>
+                  </span>
+                  <p>123, ZindaBazar, Sylhet, Bangladesh</p>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-2'>
+                  <span>
+                    <i className='ri-phone-line'></i>
+                  </span>
+                  <p>+02-1111-1111</p>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-2'>
+                  <span>
+                    <i className='ri-mail-line'></i>
+                  </span>
+                  <p>exampletest123@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg='12'>
+            <p className='footer__copyright'>
+              Copyright {year} developed by person. All rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import products from '../assets/data/products';
 
 import Helmet from '../components/Helmet/Helmet';
 import '../styles/home.css';
@@ -9,8 +10,49 @@ import '../styles/home.css';
 import { Col, Container, Row } from 'reactstrap';
 import heroImg from '../assets/images/hero-img.png';
 
+import Services from '../services/Services';
+import ProductsList from '../components/UI/ProductsList';
+import Clock from '../components/UI/Clock';
+
+import counterImg from '../assets/images/counter-timer-img.png';
+
 const Home = () => {
+  const [trendingProducts, setTrendingProducts] = useState([]);
+  const [bestSalesProducts, setBestSalesProducts] = useState([]);
+  const [mobileProducts, setMobileProducts] = useState([]);
+  const [wielessProducts, setWielessProducts] = useState([]);
+  const [popularProducts, setPopularProducts] = useState([]);
+
   const year = new Date().getFullYear();
+
+  useEffect(() => {
+    const filteredTrendingProducts = products.filter(
+      (item) => item.category === 'chair'
+    );
+
+    const filteredBestSalesProducts = products.filter(
+      (item) => item.category === 'sofa'
+    );
+
+    const filteredMobileProducts = products.filter(
+      (item) => item.category === 'mobile'
+    );
+
+    const filteredWirelessProducts = products.filter(
+      (item) => item.category === 'wireless'
+    );
+
+    const filteredPopularProducts = products.filter(
+      (item) => item.category === 'watch'
+    );
+
+    setTrendingProducts(filteredTrendingProducts);
+    setBestSalesProducts(filteredBestSalesProducts);
+    setMobileProducts(filteredMobileProducts);
+    setWielessProducts(filteredWirelessProducts);
+    setPopularProducts(filteredPopularProducts);
+  }, []);
+
   return (
     <Helmet title={'Home'}>
       <section className='hero__section'>
@@ -35,6 +77,76 @@ const Home = () => {
                 <img src={heroImg} alt='heroImg' />
               </div>
             </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <Services />
+
+      <section className='trending__products'>
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center'>
+              <h2 className='section__title'>트렌드 상품</h2>
+            </Col>
+            <ProductsList data={trendingProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className='best__sales'>
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center'>
+              <h2 className='section__title'>베스트 상품</h2>
+            </Col>
+            <ProductsList data={bestSalesProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className='timer__count'>
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <div className='clock__top-content'>
+                <h4 className='text-white fs-6 mb-2'>Limited Offers</h4>
+                <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
+              </div>
+              <Clock />
+              <motion.button
+                whileTap={{ scale: 1.2 }}
+                className='buy__btn store__btn'
+              >
+                <Link to='/shop'>쇼핑하기</Link>
+              </motion.button>
+            </Col>
+            <Col lg='6' md='6' className='text-end'>
+              <img src={counterImg} alt='counterImg' />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='new__arrivals'>
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center mb-5'>
+              <h2 className='section__title'>신제품</h2>
+            </Col>
+            <ProductsList data={mobileProducts} />
+            <ProductsList data={wielessProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className='popular__category'>
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center mb-5'>
+              <h2 className='section__title'>인기제품</h2>
+            </Col>
+            <ProductsList data={popularProducts} />
           </Row>
         </Container>
       </section>
