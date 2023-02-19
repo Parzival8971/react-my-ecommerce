2a5d1b3762045e9f8504f76254562c28890246aa
Parzival8971 <esh8971@gmail.com>
2023-02-20 00:36:53 +0900

    Feat(home): cartItem add for redux toolkit

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -5,7 +5,26 @@ import '../../styles/productCard.css';
 import { Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../redux/slices/cartSlice';
+import { toast } from 'react-toastify';
+
 const ProductCard = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(
+      cartActions.addItem({
+        id: item.id,
+        productName: item.productName,
+        price: item.price,
+        image: item.imgUrl,
+      })
+    );
+
+    toast.success('쇼핑카트에 추가되었습니다.');
+  };
+
   return (
     <Col lg='3' md='6' className='mb-2'>
       <div className='product__item'>
@@ -24,7 +43,7 @@ const ProductCard = ({ item }) => {
         </div>
         <div className='product__card-bottom d-flex align-items-center justify-content-between p-2'>
           <span className='price'>${item.price.toLocaleString('ko-KR')}</span>
-          <motion.span whileTap={{ scale: 1.2 }}>
+          <motion.span whileTap={{ scale: 1.2 }} onClick={addToCart}>
             <i className='ri-add-line'></i>
           </motion.span>
         </div>
